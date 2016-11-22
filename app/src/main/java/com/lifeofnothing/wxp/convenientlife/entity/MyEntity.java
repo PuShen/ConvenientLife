ef47d41ba2d6d1f4b82e84482c12bebb736e965b
PuShen <2876227630@qq.com>
2016-11-22 08:52:46 +0800

    pakcage init01

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/entity/MyEntity.java
@@ -0,0 +1,9 @@
+package com.lifeofnothing.wxp.convenientlife.entity;
+
+/**
+ * Created by a on 2016/11/22.
+ */
+
+public class MyEntity {
+    String manager="王晓普";
+}
---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/http/MyHttp.java
@@ -0,0 +1,9 @@
+package com.lifeofnothing.wxp.convenientlife.http;
+
+/**
+ * Created by a on 2016/11/22.
+ */
+
+public class MyHttp {
+    String manager="王晓普";
+}
