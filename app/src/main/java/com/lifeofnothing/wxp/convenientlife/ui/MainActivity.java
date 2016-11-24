19009c2715770c135e60ba5aa6124c981aba345f
PuShen <2876227630@qq.com>
2016-11-24 09:04:47 +0800

    新闻页的ListView测试

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/MainActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/MainActivity.java
@@ -2,14 +2,27 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.lifeofnothing.wxp.convenientlife.R;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mLvList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mLvList= (ListView) findViewById(R.id.LvMainList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
