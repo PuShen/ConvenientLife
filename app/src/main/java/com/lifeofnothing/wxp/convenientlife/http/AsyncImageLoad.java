51827e893306ba1c3a801af5650cf0e4a6cbd76f
liuxuwei <972090394@qq.com>
2016-11-24 21:08:02 +0800

    前两次Java文件提交发生错误  重新提交关于异步加载图片的Java文件

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/http/AsyncImageLoad.java
@@ -0,0 +1,45 @@
+package com.lifeofnothing.wxp.convenientlife.http;
+
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by 我需要一枚好辅助丶 on 2016/11/24.
+ */
+
+public class AsyncImageLoad extends AsyncTask<Void,Void,Drawable> {
+    private String mUrl;
+    private Drawable mImage;
+    private ImageView mImageview;
+
+    public AsyncImageLoad(String mUrl, ImageView mImageview) {
+        this.mUrl = mUrl;
+        this.mImageview = mImageview;
+    }
+
+    @Override
+    protected Drawable doInBackground(Void... params) {
+        try {
+            mImage = Drawable.createFromStream((InputStream)(new URL(mUrl)).openStream(), "test");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Drawable drawable) {
+        super.onPostExecute(drawable);
+        mImageview.setImageDrawable(mImage);
+    }
+}
