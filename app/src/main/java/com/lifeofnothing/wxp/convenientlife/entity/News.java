df3fc7eb6caa930104c68e825fadb0a88f56448c
修xiukunchao <choris_xiu@163.com>
2016-11-24 10:15:40 +0800

    实体类中的新闻类

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/entity/News.java
@@ -0,0 +1,73 @@
+package com.lifeofnothing.wxp.convenientlife.entity;
+
+/**
+ * Created by dell on 2016/11/24.
+ */
+
+public class News {
+    private String mTitle;
+    private String mAuthor;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmUrl1() {
+        return mUrl1;
+    }
+
+    public void setmUrl1(String mUrl1) {
+        this.mUrl1 = mUrl1;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public void setmAuthor(String mAuthor) {
+        this.mAuthor = mAuthor;
+    }
+
+    public String getmImage1() {
+        return mImage1;
+    }
+
+    public void setmImage1(String mImage1) {
+        this.mImage1 = mImage1;
+    }
+
+    public String getmUrl2() {
+        return mUrl2;
+    }
+
+    public void setmUrl2(String mUrl2) {
+        this.mUrl2 = mUrl2;
+    }
+
+    public String getmUmage2() {
+        return mUmage2;
+    }
+
+    public void setmUmage2(String mUmage2) {
+        this.mUmage2 = mUmage2;
+    }
+
+    public int getmData() {
+        return mData;
+    }
+
+    public void setmData(int mData) {
+        this.mData = mData;
+    }
+
+    private  String mUrl1;
+    private String mImage1;
+    private String mUrl2;
+    private String mUmage2;
+    private int mData;
+
+}
