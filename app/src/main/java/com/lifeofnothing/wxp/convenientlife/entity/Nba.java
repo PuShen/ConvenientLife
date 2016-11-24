d2bcdd3c2317fb631875e83f23faf5ae97bfc37d
wangxu07 <1132669228@qq.com>
2016-11-24 15:59:57 +0800

    NBA赛事的修改

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/entity/Nba.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/entity/Nba.java
@@ -1,264 +1,37 @@
 package com.lifeofnothing.wxp.convenientlife.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/11/24.
  */
 
 public class Nba{
-    private  String link1text;
-    private  String link1url;
-    private  String link2text;
-    private  String link2url;
-    private  String player1;
-    private  String player1logo;
-    private  String player1url;
-    private  String player2;
-    private  String player2logo;
-    private  String player2logobig;
-    private  String player2url;
-    private  String score;
-    private  String status;
-    private  String time;
-    private  String text;
-    private  String url;
-    private  String data;
-    private  String liveurl;
-    private  String player1info;
-    private  String player1location;
-    private  String player2info;
-    private  String player2location;
-    private  String titile;
-    private  String videoicon;
-    private  String name;
+   private List<NbaList> llist=new ArrayList<>();
+   private  List<NbaTeamMatch> ltmatch=new ArrayList<>();
 
     public Nba() {
     }
 
-    public Nba(String link1text, String link1url, String link2text, String link2url, String player1, String player1logo, String player1url, String player2, String player2logo, String player2logobig, String player2url, String score, String status, String time, String text, String url, String liveurl, String data, String player1info, String player1location, String player2info, String player2location, String titile, String videoicon, String name) {
-        this.link1text = link1text;
-        this.link1url = link1url;
-        this.link2text = link2text;
-        this.link2url = link2url;
-        this.player1 = player1;
-        this.player1logo = player1logo;
-        this.player1url = player1url;
-        this.player2 = player2;
-        this.player2logo = player2logo;
-        this.player2logobig = player2logobig;
-        this.player2url = player2url;
-        this.score = score;
-        this.status = status;
-        this.time = time;
-        this.text = text;
-        this.url = url;
-        this.liveurl = liveurl;
-        this.data = data;
-        this.player1info = player1info;
-        this.player1location = player1location;
-        this.player2info = player2info;
-        this.player2location = player2location;
-        this.titile = titile;
-        this.videoicon = videoicon;
-        this.name = name;
+    public Nba(List<NbaList> llist, List<NbaTeamMatch> ltmatch) {
+        this.llist = llist;
+        this.ltmatch = ltmatch;
     }
 
-    public String getLink1text() {
-        return link1text;
+    public List<NbaList> getLlist() {
+        return llist;
     }
 
-    public void setLink1text(String link1text) {
-        this.link1text = link1text;
+    public void setLlist(List<NbaList> llist) {
+        this.llist = llist;
     }
 
-    public String getLink1url() {
-        return link1url;
+    public List<NbaTeamMatch> getLtmatch() {
+        return ltmatch;
     }
 
-    public void setLink1url(String link1url) {
-        this.link1url = link1url;
-    }
-
-    public String getLink2text() {
-        return link2text;
-    }
-
-    public void setLink2text(String link2text) {
-        this.link2text = link2text;
-    }
-
-    public String getLink2url() {
-        return link2url;
-    }
-
-    public void setLink2url(String link2url) {
-        this.link2url = link2url;
-    }
-
-    public String getPlayer1() {
-        return player1;
-    }
-
-    public void setPlayer1(String player1) {
-        this.player1 = player1;
-    }
-
-    public String getPlayer1url() {
-        return player1url;
-    }
-
-    public void setPlayer1url(String player1url) {
-        this.player1url = player1url;
-    }
-
-    public String getPlayer1logo() {
-        return player1logo;
-    }
-
-    public void setPlayer1logo(String player1logo) {
-        this.player1logo = player1logo;
-    }
-
-    public String getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer2(String player2) {
-        this.player2 = player2;
-    }
-
-    public String getPlayer2logo() {
-        return player2logo;
-    }
-
-    public void setPlayer2logo(String player2logo) {
-        this.player2logo = player2logo;
-    }
-
-    public String getPlayer2logobig() {
-        return player2logobig;
-    }
-
-    public void setPlayer2logobig(String player2logobig) {
-        this.player2logobig = player2logobig;
-    }
-
-    public String getPlayer2url() {
-        return player2url;
-    }
-
-    public void setPlayer2url(String player2url) {
-        this.player2url = player2url;
-    }
-
-    public String getScore() {
-        return score;
-    }
-
-    public void setScore(String score) {
-        this.score = score;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String getLiveurl() {
-        return liveurl;
-    }
-
-    public void setLiveurl(String liveurl) {
-        this.liveurl = liveurl;
-    }
-
-    public String getPlayer1info() {
-        return player1info;
-    }
-
-    public void setPlayer1info(String player1info) {
-        this.player1info = player1info;
-    }
-
-    public String getPlayer1location() {
-        return player1location;
-    }
-
-    public void setPlayer1location(String player1location) {
-        this.player1location = player1location;
-    }
-
-    public String getPlayer2info() {
-        return player2info;
-    }
-
-    public void setPlayer2info(String player2info) {
-        this.player2info = player2info;
-    }
-
-    public String getPlayer2location() {
-        return player2location;
-    }
-
-    public void setPlayer2location(String player2location) {
-        this.player2location = player2location;
-    }
-
-    public String getTitile() {
-        return titile;
-    }
-
-    public void setTitile(String titile) {
-        this.titile = titile;
-    }
-
-    public String getVideoicon() {
-        return videoicon;
-    }
-
-    public void setVideoicon(String videoicon) {
-        this.videoicon = videoicon;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setLtmatch(List<NbaTeamMatch> ltmatch) {
+        this.ltmatch = ltmatch;
     }
 }
