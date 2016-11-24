a191d9dd5f48118293e4afd36518a7aeba4a8d5a
修xiukunchao <choris_xiu@163.com>
2016-11-24 15:05:43 +0800

    实体类中的足球联赛类

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/entity/LeagueFootballMatch.java
@@ -0,0 +1,181 @@
+package com.lifeofnothing.wxp.convenientlife.entity;
+
+/**
+ * Created by dell on 2016/11/24.
+ */
+
+public class LeagueFootballMatch {
+    private String mSaiCheng1;
+    private String mSaiCheng2;
+    private String mSaiCheng3;
+    private String mJiFenBang;
+    private String mC1;
+    private String mC2;
+    private String mC3;
+    private String mC4T1;
+    private String mC4T1Url;
+    private String mC4R;
+    private String mC4T2;
+    private String mC4T2Url;
+    private String mC51;
+    private String mC51Link;
+    private String mC52;
+    private String mC52Link;
+    private String mLived;
+
+    public LeagueFootballMatch(String mSaiCheng1, String mSaiCheng2, String mSaiCheng3, String mJiFenBang, String mC1, String mC2, String mC3, String mC4T1, String mC4T1Url, String mC4R, String mC4T2, String mC4T2Url, String mC51, String mC51Link, String mC52, String mC52Link, String mLived) {
+        this.mSaiCheng1 = mSaiCheng1;
+        this.mSaiCheng2 = mSaiCheng2;
+        this.mSaiCheng3 = mSaiCheng3;
+        this.mJiFenBang = mJiFenBang;
+        this.mC1 = mC1;
+        this.mC2 = mC2;
+        this.mC3 = mC3;
+        this.mC4T1 = mC4T1;
+        this.mC4T1Url = mC4T1Url;
+        this.mC4R = mC4R;
+        this.mC4T2 = mC4T2;
+        this.mC4T2Url = mC4T2Url;
+        this.mC51 = mC51;
+        this.mC51Link = mC51Link;
+        this.mC52 = mC52;
+        this.mC52Link = mC52Link;
+        this.mLived = mLived;
+    }
+
+    public String getmSaiCheng1() {
+        return mSaiCheng1;
+    }
+
+    public void setmSaiCheng1(String mSaiCheng1) {
+        this.mSaiCheng1 = mSaiCheng1;
+    }
+
+    public String getmSaiCheng2() {
+        return mSaiCheng2;
+    }
+
+    public void setmSaiCheng2(String mSaiCheng2) {
+        this.mSaiCheng2 = mSaiCheng2;
+    }
+
+    public String getmSaiCheng3() {
+        return mSaiCheng3;
+    }
+
+    public void setmSaiCheng3(String mSaiCheng3) {
+        this.mSaiCheng3 = mSaiCheng3;
+    }
+
+    public String getmJiFenBang() {
+        return mJiFenBang;
+    }
+
+    public void setmJiFenBang(String mJiFenBang) {
+        this.mJiFenBang = mJiFenBang;
+    }
+
+    public String getmC1() {
+        return mC1;
+    }
+
+    public void setmC1(String mC1) {
+        this.mC1 = mC1;
+    }
+
+    public String getmC2() {
+        return mC2;
+    }
+
+    public void setmC2(String mC2) {
+        this.mC2 = mC2;
+    }
+
+    public String getmC3() {
+        return mC3;
+    }
+
+    public void setmC3(String mC3) {
+        this.mC3 = mC3;
+    }
+
+    public String getmC4T1() {
+        return mC4T1;
+    }
+
+    public void setmC4T1(String mC4T1) {
+        this.mC4T1 = mC4T1;
+    }
+
+    public String getmC4T1Url() {
+        return mC4T1Url;
+    }
+
+    public void setmC4T1Url(String mC4T1Url) {
+        this.mC4T1Url = mC4T1Url;
+    }
+
+    public String getmC4R() {
+        return mC4R;
+    }
+
+    public void setmC4R(String mC4R) {
+        this.mC4R = mC4R;
+    }
+
+    public String getmC4T2() {
+        return mC4T2;
+    }
+
+    public void setmC4T2(String mC4T2) {
+        this.mC4T2 = mC4T2;
+    }
+
+    public String getmC4T2Url() {
+        return mC4T2Url;
+    }
+
+    public void setmC4T2Url(String mC4T2Url) {
+        this.mC4T2Url = mC4T2Url;
+    }
+
+    public String getmC51() {
+        return mC51;
+    }
+
+    public void setmC51(String mC51) {
+        this.mC51 = mC51;
+    }
+
+    public String getmC51Link() {
+        return mC51Link;
+    }
+
+    public void setmC51Link(String mC51Link) {
+        this.mC51Link = mC51Link;
+    }
+
+    public String getmC52() {
+        return mC52;
+    }
+
+    public void setmC52(String mC52) {
+        this.mC52 = mC52;
+    }
+
+    public String getmC52Link() {
+        return mC52Link;
+    }
+
+    public void setmC52Link(String mC52Link) {
+        this.mC52Link = mC52Link;
+    }
+
+    public String getmLived() {
+        return mLived;
+    }
+
+    public void setmLived(String mLived) {
+        this.mLived = mLived;
+    }
+}
