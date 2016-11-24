9029a076b4643be7c2f7f89949ce60b9d20200a0
PuShen <2876227630@qq.com>
2016-11-24 15:25:06 +0800

    新闻页的界面优化

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/MainActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/MainActivity.java
@@ -1,14 +1,25 @@
 package com.lifeofnothing.wxp.convenientlife.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.lifeofnothing.wxp.convenientlife.R;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mLvList;
+    private TextView mTvSearch;
 
+    private View.OnClickListener listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this,NewstypeActivity.class);
+            startActivityForResult(intent,0);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         mLvList= (ListView) findViewById(R.id.LvMainList);
+        mTvSearch= (TextView) findViewById(R.id.TvItemnewsSearch);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mTvSearch.setOnClickListener(listener);
     }
 }
