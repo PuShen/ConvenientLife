719ba4da64ee70b5b6ac87ddb480f2c6ea5eadd1
PuShen <2876227630@qq.com>
2016-11-24 14:45:57 +0800

    Merge branch 'master' of D:\ConvenientLife with conflicts.

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/NewstypeActivity.java
@@ -0,0 +1,18 @@
+package com.lifeofnothing.wxp.convenientlife.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.lifeofnothing.wxp.convenientlife.R;
+
+/**
+ * Created by a on 2016/11/24.
+ */
+
+public class NewstypeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiry_newstype);
+    }
+}
