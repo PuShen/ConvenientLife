4ba609fea8b18373e0caba2dbb5891ed2ad7e5a0
刘超 <1140053683@qq.com>
2016-11-25 15:15:58 +0800

    将NBAParser.java文件下Nbaparser的代码放到了创建的NBAparser中

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/prasor/NBAParser.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/prasor/NBAParser.java
@@ -5,56 +5,53 @@ import com.lifeofnothing.wxp.convenientlife.entity.Nba;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 /**
- * Created by Administrator on 2016/11/23.
+ * Created by 刘超 on 2016/11/25.
  */
 
-public class NbaParser {
-    //源Json串
-    private String mSource = null;
+public class NBAParser {
+        //源Json串
+        private String mSource = null;
 
-    public NbaParser(String source) {
-        this.mSource = source;
-    }
+        public NBAParser(String source) {
+            this.mSource = source;
+        }
 
-    public Nba parse() {
-        //用于保存服务器返回的数据
-        Nba nba = null;
-        JSONObject jsonObject = null;
+        public Nba parse() {
+            //用于保存服务器返回的数据
+            Nba nba = null;
+            JSONObject jsonObject = null;
 
-        //获取服务器返回的状态
-        String reason = null;
-        try {
-            if (mSource != null) {
-                jsonObject = new JSONObject(mSource);
-                reason = jsonObject.getString("reason");
+            //获取服务器返回的状态
+            String reason = null;
+            try {
+                if (mSource != null) {
+                    jsonObject = new JSONObject(mSource);
+                    reason = jsonObject.getString("reason");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
+            //检查服务器返回的状态,若成功则开始数据解析
+            if (reason.equals("查询成功")) {
+                //开始解析
+                nba =
+                        startParse(jsonObject);
+
+
+            } else {
+                //否则赋值为null
+                nba = null;
+            }
+
+            return nba;
         }
-        //检查服务器返回的状态,若成功则开始数据解析
-        if (reason.equals("查询成功")) {
-            //开始解析
-            nba =
-                    startParse(jsonObject);
 
+        private Nba startParse(JSONObject jsonObject) {
+            //开始解析主要数据
+            Nba nba = new Nba();
+            return nba;
 
-        } else {
-            //否则赋值为null
-            nba = null;
-        }
-
-        return nba;
     }
-
-    private Nba startParse(JSONObject jsonObject) {
-        //开始解析主要数据
-        Nba nba = new Nba();
-
-        return nba;
-    }
-
-
 }
