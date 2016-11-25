a03af8e99b7fce07d8683b514a06680b43dc0aaf
PuShen <wangxiaopu19960301@qq.com>
2016-11-25 15:27:53 +0800

    bug优化和界面优化

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/NewsActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/NewsActivity.java
@@ -3,6 +3,7 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
