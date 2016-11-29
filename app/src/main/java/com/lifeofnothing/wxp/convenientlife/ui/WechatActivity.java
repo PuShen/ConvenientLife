fae1aca71f564640ad51c9f85b934fc23ac50c92
PuShen <wangxiaopu19960301@qq.com>
2016-11-29 09:18:55 +0800

    微信精选界面的初始化

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WechatActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WechatActivity.java
@@ -1,9 +1,10 @@
 package com.lifeofnothing.wxp.convenientlife.ui;
 
-import android.app.SearchManager;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.lifeofnothing.wxp.convenientlife.R;
 
@@ -12,9 +13,35 @@ import com.lifeofnothing.wxp.convenientlife.R;
  */
 
 public class WechatActivity extends Activity {
+    private ImageView mIvBack;
+    private ListView mLvList;
+    private View.OnClickListener listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.IvWechatBack:
+                    finish();
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wechat);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mIvBack= (ImageView) findViewById(R.id.IvWechatBack);
+        mLvList= (ListView) findViewById(R.id.LvWechatList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIvBack.setOnClickListener(listener);
     }
 }
