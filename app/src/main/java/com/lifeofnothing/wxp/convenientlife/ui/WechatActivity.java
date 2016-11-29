98cefa71163b38bf328e9ef10de901881b84e9f0
PuShen <wangxiaopu19960301@qq.com>
2016-11-29 08:40:00 +0800

    微信精选界面的初始化

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WechatActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WechatActivity.java
@@ -5,6 +5,8 @@ import android.app.SearchManager;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.lifeofnothing.wxp.convenientlife.R;
+
 /**
  * Created by dell on 2016/11/28.
  */
@@ -13,5 +15,6 @@ public class WechatActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wechat);
     }
 }
