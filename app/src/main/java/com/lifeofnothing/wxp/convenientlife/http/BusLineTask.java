7e44dfdc8a5e8918ba160df025c5509a83db5e0f
zhaohang95 <313283149@qq.com>
2016-11-30 08:53:22 +0800

    URL 地址拼接修改

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/http/BusLineTask.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/http/BusLineTask.java
@@ -25,21 +25,32 @@ public class BusLineTask {
     private String mParam1 = "key=580f5ec11f82f6f5c19fb9321c0ca157&city=";
     private String mParam2;     //城市代码或城市名称
     private String mParam3;     //公交车号
-
+    private String url;
     public BusLineTask(String city,String bus){
         this.mParam2 = city;
         this.mParam3 = bus;
     }
 
-    private String url = mUrl+mParam1+mParam2+"&%20bus="+mParam3;
+
     private List<BusLine> Buslist = new ArrayList<>();
     public void Bus_run(){
+        url= mUrl+mParam1+mParam2+"&%20bus="+mParam3;
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url,new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 String str = Buslist.toString();
                 BusLineParser bp = new BusLineParser(str);
+
+                try {
+                    String s = response.getString("reason");
+                    Log.e("reason",response.toString());
+                    if (s.equals("success")){
+                        Log.e("reason","1");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 super.onSuccess(statusCode, headers, response);
             }
         });
