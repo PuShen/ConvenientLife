9e6dd7249d509dc6728b7dada5b7941cb7b3936a
zhaohang95 <313283149@qq.com>
2016-11-30 10:48:44 +0800

    足球联赛task和历史上的今天的修改

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/http/LeagueFootballMatchTask.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/http/LeagueFootballMatchTask.java
@@ -2,8 +2,16 @@ package com.lifeofnothing.wxp.convenientlife.http;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
-import com.lifeofnothing.wxp.convenientlife.prasor.TodayInHisoryParser;
+import com.lifeofnothing.wxp.convenientlife.prasor.leagueFootballMacthParser;
+import com.lifeofnothing.wxp.convenientlife.entity.LeagueFootballMatch;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,55 +19,56 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by hang on 2016/11/29.
+ * Created by hang on 2016/11/30.
  */
 
-public class TodayInHistoryTask implements Runnable{
+public class LeagueFootballMatchTask implements Runnable{
+    private String mUrl = "http://op.juhe.cn/onebox/football/league?key=054f6720b0fb04e65f1afd424aa460f3&league=";
+    private String mParam;      //联赛名称
+    private String url;
+    private Handler mHandler;
 
-    private String mUrl = "http://api.juheapi.com/japi/toh?key=您申请的KEY&v=1.0";
-    private String mParam;      //月份
-    private String mParam1;     //日
-    private String mMonth = "&month=";
-    private String mDay = "&day=";
-    private Handler handler;
-
-    public TodayInHistoryTask(String month, String day, Handler mHandler){
-        this.mParam = month;
-        this.mParam1 = day;
-        this.handler = mHandler;
+    public LeagueFootballMatchTask(String league,Handler handler){
+        this.mParam = league;
+        this.mHandler = handler;
     }
+
 
     @Override
     public void run() {
+        url = mUrl+mParam;
         HttpURLConnection connection = null;
         BufferedReader reader = null;
         try {
-            URL url = new URL(mUrl+mMonth+mParam+mDay+mParam1);
-            connection = (HttpURLConnection)url.openConnection();
+            URL u = new URL(url);
+            connection = (HttpURLConnection)u.openConnection();
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuffer buffer = new StringBuffer("");
             String line = null;
-            while (null != (line = reader.readLine())){
+            while (null!=(line = reader.readLine())){
                 buffer.append(line);
             }
             Message message = new Message();
             message.what = 0;
-            message.obj = TodayInHisoryParser.getGson(buffer);
-            handler.sendMessage(message);
+            message.obj = leagueFootballMacthParser.getGson(buffer);
+            mHandler.sendMessage(message);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (reader != null){
+            if (null!=reader){
                 try {
                     reader.close();
                     connection.disconnect();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
             }
             if (null != connection){
                 connection.disconnect();
