f5789ce2df6efef4cd3ebf8993ac8ad406e5b44a
修xiukunchao <choris_xiu@163.com>
2016-12-01 09:45:39 +0800

    更改天气预报

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WeatherActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WeatherActivity.java
@@ -1,12 +1,18 @@
 package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.lifeofnothing.wxp.convenientlife.R;
 
 /**
  * Created by dell on 2016/12/1.
  */
 
 public class WeatherActivity extends Activity {
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
 }
