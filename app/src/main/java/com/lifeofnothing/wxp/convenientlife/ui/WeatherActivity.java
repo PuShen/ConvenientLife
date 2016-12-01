8d3fad787324d0285f5b582e8ad9264de1e43b6c
修xiukunchao <choris_xiu@163.com>
2016-12-01 08:57:27 +0800

    天气预报

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/WeatherActivity.java
@@ -0,0 +1,12 @@
+package com.lifeofnothing.wxp.convenientlife.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by dell on 2016/12/1.
+ */
+
+public class WeatherActivity extends Activity {
+
+
+}
