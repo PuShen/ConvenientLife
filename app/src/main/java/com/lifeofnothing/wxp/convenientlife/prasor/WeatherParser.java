9dae25b415371a711dbc635bdb90dbe40efed251
dadaxixi <632283554@qq.com>
2016-12-05 15:09:26 +0800

    WeatherParser update.

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/prasor/WeatherParser.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/prasor/WeatherParser.java
@@ -1,6 +1,5 @@
 package com.lifeofnothing.wxp.convenientlife.prasor;
 
-import com.lifeofnothing.wxp.convenientlife.entity.LWeather;
 import com.lifeofnothing.wxp.convenientlife.entity.Weather;
 
 import org.json.JSONArray;
@@ -22,9 +21,9 @@ public class WeatherParser {
         this.mSource = source;
     }
 
-    public Weather parse() {
+    public List<Weather> parse() {
         //用于保存服务器返回的数据
-        Weather weather = null;
+        List<Weather> weathers = null;
 
         //获取服务器返回的状态
         int errorCode = -1;
@@ -36,134 +35,119 @@ public class WeatherParser {
                 //检查服务器返回的状态,若成功则开始数据解析
                 if (0 == errorCode) {
                     //开始解析
-                    weather =
-                            startParse(jsonObject.getJSONObject("result"));
+                   weathers =
+                    startParse(jsonObject.getJSONObject("result"));
                 } else {
                     //否则赋值为null
-                    weather = null;
+                    weathers = null;
                 }
             }
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
         }
-        return weather;
+        return weathers;
     }
 
-    private Weather startParse(JSONObject result) {
+    private List<Weather> startParse(JSONObject result) {
         //开始解析主要数据
-        Weather weather = new Weather();
+        List<Weather> weathers = new ArrayList<Weather>();
+        Weather weather = null;
         try {
             JSONObject data = result.getJSONObject("data");
-
-            //实时天气部分
+            String pubDate = data.getString("pubdate");
             JSONObject realtime = data.getJSONObject("realtime");
-            String cityCode = realtime.getString("city_code");
-            weather.setCity_code(cityCode);
             String cityName = realtime.getString("city_name");
             weather.setCity_name(cityName);
-            String date = realtime.getString("data");
-            weather.setDate(date);
-            String time = realtime.getString("time");
-            weather.setTime(time);
+
+            weather = new Weather(cityName, pubDate);
+
             String week = realtime.getString("week");
             weather.setWeek(week);
-            String moon = realtime.getString("moon");
-            weather.setMoon(moon);
-            Double dataUptime = realtime.getDouble("dataUptime");
-            weather.setDataUptime(Double.toString(dataUptime));
-
-            JSONObject weatherJSONObj = realtime.getJSONObject("weather");
-            String temperature = weatherJSONObj.getString("temperature");
+            JSONObject realtimeWeatherJSONObj = realtime.getJSONObject("weather");
+            String temperature = realtimeWeatherJSONObj.getString("temperature");
             weather.setTemperature(temperature);
-            String humidity = weatherJSONObj.getString("humidity");
+            String humidity = realtimeWeatherJSONObj.getString("humidity");
             weather.setHumidity(humidity);
-            String info = weatherJSONObj.getString("info");
+            String info = realtimeWeatherJSONObj.getString("info");
             weather.setInfo(info);
-            String img = weatherJSONObj.getString("img");
-            weather.setImg(img);
+            String img = realtimeWeatherJSONObj.getString("img");
+            int imgInt = Integer.parseInt(img);
+            weather.setImg(imgInt);
 
             JSONObject windJSONObj = realtime.getJSONObject("wind");
             String direct = windJSONObj.getString("direct");
             weather.setDirect(direct);
             String power = windJSONObj.getString("power");
             weather.setPower(power);
-            String offset = windJSONObj.getString("offset");
-            weather.setOffset(offset);
-            String windspeed = windJSONObj.getString("windspeed");
-            weather.setWindspeed(windspeed);
 
             //生活指数部分
             JSONObject life = data.getJSONObject("life");
             JSONObject infoJSONObj = life.getJSONObject("info");
             JSONArray chuanyiJSONArray = infoJSONObj.getJSONArray("chuanyi");
-            String chuanyi =
-                    chuanyiJSONArray.get(0) + "#" + chuanyiJSONArray.get(1);
+            String chuanyi = chuanyiJSONArray.get(0).toString();
             weather.setChuanyi(chuanyi);
+            String chuanyides = chuanyiJSONArray.get(1).toString();
+            weather.setChuanyides(chuanyides);
             JSONArray ganmaoJSONArray = infoJSONObj.getJSONArray("ganmao");
-            String ganmao =
-                    ganmaoJSONArray.get(0) + "#" + ganmaoJSONArray.get(1);
+            String ganmao = ganmaoJSONArray.get(0).toString();
             weather.setGanmao(ganmao);
+            String ganmaodes = ganmaoJSONArray.get(1).toString();
+            weather.setGanmaodes(ganmaodes);
             JSONArray kongtiaoJSONArray = infoJSONObj.getJSONArray("kongtiao");
-            String kongtiao =
-                    kongtiaoJSONArray.get(0) + "#" + kongtiaoJSONArray.get(1);
+            String kongtiao = kongtiaoJSONArray.get(0).toString();
             weather.setKongtiao(kongtiao);
-            JSONArray wuranJSONArray = infoJSONObj.getJSONArray("wuran");
-            String wuran =
-                    wuranJSONArray.get(0) + "#" + wuranJSONArray.get(1);
-            weather.setWuran(wuran);
+            String kongtiaodes = kongtiaoJSONArray.get(1).toString();
+            weather.setKongtiaodes(kongtiaodes);
             JSONArray xicheJSONArray = infoJSONObj.getJSONArray("xiche");
-            String xiche =
-                    xicheJSONArray.get(0) + "#" + xicheJSONArray.get(1);
+            String xiche = xicheJSONArray.get(0).toString();
             weather.setXiche(xiche);
+            String xichedes = xicheJSONArray.get(1).toString();
+            weather.setXichedes(xichedes);
             JSONArray yundongJSONArray = infoJSONObj.getJSONArray("yundong");
-            String yundong =
-                    yundongJSONArray.get(0) + "#" + yundongJSONArray.get(1);
+            String yundong = yundongJSONArray.get(0).toString();
             weather.setYundong(yundong);
+            String yundongdes = yundongJSONArray.get(1).toString();
+            weather.setYundongdes(yundongdes);
             JSONArray ziwaixianJSONArray = infoJSONObj.getJSONArray("ziwaixian");
-            String ziwaixian =
-                    ziwaixianJSONArray.get(0) + "#" + ziwaixianJSONArray.get(1);
+            String ziwaixian = ziwaixianJSONArray.get(0).toString();
             weather.setZiwaixian(ziwaixian);
+            String ziwaixiandes = ziwaixianJSONArray.get(1).toString();
+            weather.setZiwaixiandes(ziwaixiandes);
+
+            weathers.add(weather);
 
             //天气预报部分
             JSONArray weatherJSONArray = data.getJSONArray("weather");
-            List<LWeather> lWeathers = new ArrayList<LWeather>();
-            for (int i = 0; i < weatherJSONArray.length(); i++) {
-                LWeather lWeather = new LWeather();
-                JSONObject lWeatherJSONObj = weatherJSONArray.getJSONObject(i);
-                JSONObject lWeatherInfoJSONObj = lWeatherJSONObj.getJSONObject("info");
-                JSONArray dayJSONArray = lWeatherInfoJSONObj.getJSONArray("day");
-                JSONArray nightJSONArray = lWeatherInfoJSONObj.getJSONArray("night");
 
-                String dId = (String) dayJSONArray.get(0);
-                lWeather.setD_id(dId);
-                String dWeather = (String) dayJSONArray.get(1);
-                lWeather.setD_weather(dWeather);
-                String dTemperature = (String) dayJSONArray.get(2);
-                lWeather.setD_temperature(dTemperature);
-                String dDirect = (String) dayJSONArray.get(3);
-                lWeather.setD_direct(dDirect);
-                String dPower = (String) dayJSONArray.get(4);
-                lWeather.setD_power(dPower);
-                String nId = (String) nightJSONArray.get(0);
-                lWeather.setN_id(nId);
-                String nWeather = (String) nightJSONArray.get(1);
-                lWeather.setN_weather(nWeather);
-                String nTemperature = (String) nightJSONArray.get(2);
-                lWeather.setN_temperature(nTemperature);
-                String nDirect = (String) nightJSONArray.get(3);
-                lWeather.setN_direct(nDirect);
-                String nPower = (String) nightJSONArray.get(4);
-                lWeather.setN_power(nPower);
+            for (int i = 1; i < weatherJSONArray.length(); i++) {
+                Weather weather1 = new Weather(cityName, null);
 
-                lWeathers.add(lWeather);
+                JSONObject weatherJSONObj = weatherJSONArray.getJSONObject(i);
+                String date = weatherJSONObj.getString("date");
+                weather1.setDate(date);
+                JSONObject weatherInfoJSONObj = weatherJSONObj.getJSONObject("info");
+                JSONArray dayJSONArray = weatherInfoJSONObj.getJSONArray("day");
+
+                int forecastImgInt = Integer.parseInt((String) dayJSONArray.get(0));
+                weather1.setImg(forecastImgInt);
+                String forecastInfo = (String) dayJSONArray.get(1);
+                weather1.setInfo(forecastInfo);
+                String forecastTemperature = (String) dayJSONArray.get(2);
+                weather1.setTemperature(forecastTemperature);
+                String forecastDirect = (String) dayJSONArray.get(3);
+                weather1.setDirect(forecastDirect);
+                String forecastPower = (String) dayJSONArray.get(4);
+                weather1.setPower(forecastPower);
+                String forecastWeek = weatherJSONObj.getString("week");
+                weather1.setWeek(forecastWeek);
+
+                weathers.add(weather1);
             }
-            weather.setLs(lWeathers);
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        return weather;
+        return weathers;
     }
 }
