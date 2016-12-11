aed92bc1802e7b8f454fc619fdd8d90304e84417
PuShen <wangxiaopu19960301@qq.com>
2016-12-11 20:15:14 +0800

    公交界面更新

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/BusLineActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/BusLineActivity.java
@@ -2,6 +2,10 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import com.lifeofnothing.wxp.convenientlife.R;
 
@@ -10,9 +14,37 @@ import com.lifeofnothing.wxp.convenientlife.R;
  */
 
 public class BusLineActivity extends Activity {
+    private ImageView mIvBack;
+    private TextView mTvCity;
+    private ImageView mIvAdd;
+    private View.OnClickListener listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.IvBuslineBack:
+                    finish();
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busline);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mIvBack= (ImageView) findViewById(R.id.IvBuslineBack);
+        mTvCity= (TextView) findViewById(R.id.TvBuslineCity);
+        mIvAdd= (ImageView) findViewById(R.id.IvBuslineAdd);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIvBack.setOnClickListener(listener);
     }
 }
