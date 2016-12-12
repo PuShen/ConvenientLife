a3c55fd9da2e0b6f476bf892755dab712d7c6a80
wangxu07 <1132669228@qq.com>
2016-12-12 15:37:26 +0800

    周公解梦Activity

---
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/ExplainDream.java
@@ -0,0 +1,15 @@
+package com.lifeofnothing.wxp.convenientlife.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+
+public class ExplainDream extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
