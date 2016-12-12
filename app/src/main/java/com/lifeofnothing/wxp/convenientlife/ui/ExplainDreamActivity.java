29d6a116bcb2c01f7213927c04cda28d094eeb1a
wangxu07 <1132669228@qq.com>
2016-12-12 22:10:24 +0800

    周公解梦xml修改

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/ExplainDreamActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/ExplainDreamActivity.java
@@ -2,14 +2,22 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 /**
  * Created by Administrator on 2016/12/12.
  */
 
 public class ExplainDreamActivity extends Activity {
+    private ImageView IvExplainDream;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
