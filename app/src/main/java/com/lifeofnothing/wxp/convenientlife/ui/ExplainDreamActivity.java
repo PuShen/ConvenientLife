efd297fa1065d5910d52434b9501882fee7bc0b9
修xiukunchao <choris_xiu@163.com>
2016-12-13 08:18:52 +0800

    修改activity

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/ExplainDreamActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/ExplainDreamActivity.java
@@ -3,6 +3,8 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.lifeofnothing.wxp.convenientlife.R;
+
 /**
  * Created by Administrator on 2016/12/12.
  */
@@ -11,5 +13,6 @@ public class ExplainDreamActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explaindream);
     }
 }
