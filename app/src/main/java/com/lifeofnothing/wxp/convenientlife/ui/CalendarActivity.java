38e3249eb2b933b3b3209c3ab616cada0c8a434b
修xiukunchao <choris_xiu@163.com>
2016-12-15 17:14:23 +0800

    获取网络数据

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/CalendarActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/CalendarActivity.java
@@ -2,20 +2,27 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lifeofnothing.wxp.convenientlife.R;
 import com.lifeofnothing.wxp.convenientlife.entity.CalendarView;
 import com.lifeofnothing.wxp.convenientlife.entity.ClickDataListener;
+import com.lifeofnothing.wxp.convenientlife.entity.PerpetualCalendar;
+import com.lifeofnothing.wxp.convenientlife.http.PerpetualCalendarTask;
 
 /**
  * 日历显示activity
  */
 public class CalendarActivity extends Activity {
+	private TextView textView;
 	private ImageView mIvCaleandarback;
 	private CalendarView calendarview;
+	private PerpetualCalendar mPerpetualCalendar;
 	private View.OnClickListener listener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
@@ -37,8 +44,25 @@ public class CalendarActivity extends Activity {
 		super.onStart();
 		mIvCaleandarback= (ImageView) findViewById(R.id.IvCalendarBack);
 		calendarview = (CalendarView) findViewById(R.id.calendarview);
+		textView = (TextView)findViewById(R.id.first);
+		textView = (TextView)findViewById(R.id.second);
+		textView = (TextView)findViewById(R.id.three);
+		textView = (TextView)findViewById(R.id.four);
+		textView = (TextView)findViewById(R.id.five);
+		textView = (TextView)findViewById(R.id.six);
+		textView = (TextView)findViewById(R.id.seven);
 	}
-
+	private Handler mHandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			switch (msg.what){
+				case 0:
+					mPerpetualCalendar = (PerpetualCalendar) msg.obj;
+					break;
+			}
+		}
+	};
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -49,6 +73,7 @@ public class CalendarActivity extends Activity {
 			public void clickData(String year, String month, String day) {
 				Toast.makeText(getApplicationContext(),
 						year + "-" + month + "-" + day, Toast.LENGTH_SHORT).show();
+				new Thread(new PerpetualCalendarTask(year + "-" + month + "-" + day,mHandler)).start();
 
 			}
 		});
