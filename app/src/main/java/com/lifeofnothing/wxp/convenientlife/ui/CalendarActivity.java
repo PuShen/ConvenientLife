9920eb313ba3adc6a173253cb51977f10b07b276
修xiukunchao <choris_xiu@163.com>
2016-12-15 16:01:58 +0800

    设置监听器

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/CalendarActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/CalendarActivity.java
@@ -2,6 +2,8 @@ package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.lifeofnothing.wxp.convenientlife.R;
@@ -12,13 +14,35 @@ import com.lifeofnothing.wxp.convenientlife.entity.ClickDataListener;
  * 日历显示activity
  */
 public class CalendarActivity extends Activity {
-
+	private ImageView mIvCaleandarback;
+	private CalendarView calendarview;
+	private View.OnClickListener listener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+				case R.id.IvCalendarBack:
+					finish();
+					break;
+			}
+		}
+	};
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calendarmain);
-		CalendarView calendarview = (CalendarView) findViewById(R.id.calendarview);
-		//设置点击日期的事件监听
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mIvCaleandarback= (ImageView) findViewById(R.id.IvCalendarBack);
+		calendarview = (CalendarView) findViewById(R.id.calendarview);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mIvCaleandarback.setOnClickListener(listener);
 		calendarview.setClickDataListener(new ClickDataListener() {
 
 			@Override
@@ -28,6 +52,6 @@ public class CalendarActivity extends Activity {
 
 			}
 		});
-	}
 
+	}
 }
