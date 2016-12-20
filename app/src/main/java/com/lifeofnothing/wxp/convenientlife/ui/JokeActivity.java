915f77ff43c4bfa55dbe4206687b63aa20b75a79
liuxuwei <972090394@qq.com>
2016-12-20 10:34:10 +0800

    笑话大全第八次完善——关于网络状况的判断

--- app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/JokeActivity.java
+++ app/src/main/java/com/lifeofnothing/wxp/convenientlife/ui/JokeActivity.java
@@ -1,7 +1,10 @@
 package com.lifeofnothing.wxp.convenientlife.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Bundle;
@@ -77,6 +80,32 @@ public class JokeActivity extends Activity {
             }
         }
     };
+
+    public class NetWorkUtil {
+        private Context context = null;
+        public NetWorkUtil(Context context) {
+            this.context = context;
+        }
+        /**
+         * 判断当前网络是否可用
+         *
+         * @return
+         */
+        public boolean isNetWorkAvailable() {
+            ConnectivityManager manager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (manager != null) {
+                NetworkInfo info = manager.getActiveNetworkInfo();
+                if (info != null && info.isConnected()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
